c1d7a5a97c80fbdf3a5aff584cfcbc9b6346ed50
Miriam Jimenez <132669780+miriamjimenezj@users.noreply.github.com>
2023-11-23 15:12:17 +0000

    Relate pages

--- src/Solopods/routes/index.js
+++ src/Solopods/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Solopods' });
 });
 
+router.get('/products', (req, res) => {
+  res.render('products', { title: 'Solopods' });
+});
+
+router.get('/services', (req, res) => {
+  res.render('services', { title: 'Solopods' });
+});
+
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'Solopods' });
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Solopods' });
+});
+
+router.get('/collabs', (req, res) => {
+  res.render('collabs', { title: 'Solopods' });
+});
+
 module.exports = router;
